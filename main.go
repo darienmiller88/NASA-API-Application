aea05189e6422ccb3b3e926d2c635926dc88600f
darienmiller88 <darienmiller88@yahoo.com>
2020-11-01 23:03:09 -0500

    checking for errors

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
-	"strings"
 
 	"time"
 
@@ -14,11 +13,14 @@ import (
 	"github.com/joho/godotenv"
 )
 
-const url string = "https://api.nasa.gov/planetary/apod?"
+const url string -9*"https://api.nasa.gov/planetary/apod?"
 
 func main() {
 	//Get a default gin router
 	app := gin.Default()
+
+	//Set project to release mode for deployment
+	gin.SetMode(gin.ReleaseMode)
 
 	//Preload all the html files so accessing them will be much faster
 	app.LoadHTMLGlob("templates/*")
@@ -29,91 +31,85 @@ func main() {
 	//Load the dependency to read in .env files.
 	errHandle(godotenv.Load())
 
-	app.GET("/",            renderCurrentAPODPicture)
-	app.GET("/api/:date",   getAPODJSON)
-	app.GET("/apod/:date",  renderPictureByDate)	
+	app.GET("/", renderCurrentAPODPicture)
+	app.GET("/api/:date", getAPODJSON)
+	app.GET("/apod/:date", renderPictureByDate)
 	app.POST("/changeDate", redirectToAPODPicture)
-	app.NoRoute(render404Page)//Handle 404 errors
+	app.NoRoute(render404Page) //Handle 404 errors
 
 	fmt.Println("Server is up!")
 	app.Run()
 }
 
-func render404Page(c *gin.Context){
+func render404Page(c *gin.Context) {
 	c.HTML(http.StatusNotFound, "404.html", gin.H{
-		"Errorcode": http.StatusNotFound,
-		"ErrorDescription" : "Page not found!",
+		"Errorcode":        http.StatusNotFound,
+		"ErrorDescription": "Page not found!",
 	})
 }
 
-func getAPODJSON(c *gin.Context){
-	webData, response  := loadAPODInfo("date=" + c.Param("date"), "&api_key=" + os.Getenv("API_KEY"))
+func getAPODJSON(c *gin.Context) {
+	webData, response := loadAPODInfo("date="+c.Param("date"), "&api_key="+os.Getenv("API_KEY"))
 
-
-	if response.StatusCode != 200{
+	if response.StatusCode != 200 {
 		c.JSON(response.StatusCode, gin.H{
-			"errmsg" : "No data found for date: " + c.Param("date"),
+			"errmsg": "No data found for date: " + c.Param("date"),
 		})
-	}else{
+	} else {
 		c.JSON(response.StatusCode, webData)
 	}
 }
 
-func redirectToAPODPicture(c *gin.Context){
-	//Once this function is hit when the user posts a date, redirect them to the above route with the date 
+func redirectToAPODPicture(c *gin.Context) {
+	//Once this function is hit when the user posts a date, redirect them to the above route with the date
 	//they entered.
-	c.Redirect(http.StatusFound, "/apod/" + c.PostForm("date"))
+	c.Redirect(http.StatusFound, "/apod/"+c.PostForm("date"))
 }
 
-func renderCurrentAPODPicture(c *gin.Context){
-	//Retrieve the current date, and format into the desired style using the below date as a placeholder. 
+func renderCurrentAPODPicture(c *gin.Context) {
+	//Retrieve the current date, and format into the desired style using the below date as a placeholder.
 	//This is needed because of possible time zone issues with the NASA api and EST, so retrieving the current
-	//data late at night results in NASA thinking I am trying to receive TOMORROW'S information, which then 
+	//data late at night results in NASA thinking I am trying to receive TOMORROW'S information, which then
 	//results in "webData" being empty.
 	date := time.Now().Format("2006-01-02")
 
 	//Access the NASA api and retrieve the most recent picture (today's date) using the date variable.
-	webData, response := loadAPODInfo("date=" + date, "&api_key=" + os.Getenv("API_KEY"))
-	var apodURL string  = webData["url"].(string)
-	var isAPicture bool = strings.Contains(apodURL, "jpg")//Check to see if the url is a jpg.
+	webData, response := loadAPODInfo("date="+date, "&api_key="+os.Getenv("API_KEY"))
 
-	renderHTML(c, response.StatusCode, webData, isAPicture)
+	renderHTML(c, response.StatusCode, webData, webData["media_type"] == "image")
 }
 
-func renderPictureByDate(c *gin.Context){
+func renderPictureByDate(c *gin.Context) {
 	//Access the NASA api and retrieve the picture according to the date the user entered.
-	webData, response := loadAPODInfo("date=" + c.Param("date"), "&api_key=" + os.Getenv("API_KEY"))
-	
+	webData, response := loadAPODInfo("date="+c.Param("date"), "&api_key="+os.Getenv("API_KEY"))
+
 	fmt.Println("repsonse:", response.StatusCode)
 	//If the response returned a status code other than 200, render the 404 html page.
-	if response.StatusCode != 200{
+	if response.StatusCode != 200 {
 		c.HTML(response.StatusCode, "404.html", gin.H{
-			"Errorcode": response.StatusCode,
-			"ErrorDescription" : "Bad Request!",
-		 })
-	}else{
-		var apodURL string  = webData["url"].(string)
-		var isAPicture bool = strings.Contains(apodURL, "jpg")//Check to see if the url is a jpg.
-
+			"Errorcode":        response.StatusCode,
+			"ErrorDescription": "Bad Request!",
+		})
+	} else {
 		//Otherwise, render the index.html file with the necessary information.
-		renderHTML(c, response.StatusCode, webData, isAPicture)
+		renderHTML(c, response.StatusCode, webData, webData["media_type"] == "image")
 	}
-} 
+}
 
-func renderHTML(c *gin.Context, StatusCode int, webData map[string]interface{}, isAPicture bool){
-	c.HTML(	
+func renderHTML(c *gin.Context, StatusCode int, webData map[string]interface{}, isAPicture bool) {
+	c.HTML(
 		StatusCode, "index.html", gin.H{
-			"URL" :         webData["url"],
-			"Description" : webData["explanation"],
-			"Date" :        webData["date"],
-			"Title" :       webData["title"],
-			"Author":       webData["copyright"],
-			"IsAPicture" : 	isAPicture,
+			"URL":         webData["url"],
+			"Description": webData["explanation"],
+			"Date":        webData["date"],
+			"Title":       webData["title"],
+			"Author":      webData["copyright"],
+			"IsAPicture":  isAPicture,
 		},
 	)
 }
 
-func loadAPODInfo(date string, apiKey string) (map[string]interface{}, *http.Response){
+func loadAPODInfo(date string, apiKey string) (map[string]interface{}, *http.Response) {
 	response, err := http.Get(url + date + apiKey)
 	errHandle(err)
 
@@ -124,12 +120,12 @@ func loadAPODInfo(date string, apiKey string) (map[string]interface{}, *http.Res
 	webData := make(map[string]interface{})
 
 	//Unload the byte array information into a hashmap.
-	errHandle(json.Unmarshal(data, &webData))	
+	errHandle(json.Unmarshal(data, &webData))
 
 	return webData, response
 }
 
-func errHandle(err error){
+func errHandle(err error) {
 	if err != nil {
 		fmt.Println(err)
 	}
